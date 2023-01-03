283d9868a5374388a179b82e5df079678b853f9d
Markus Gustafsson <markusgu111@gmail.com>
2023-01-03 11:35:04 +0100

    set up spring boot project

---
+++ puppiesApi/src/main/java/se/example/puppiesapi/PuppiesApiApplication.java
@@ -0,0 +1,13 @@
+package se.example.puppiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppiesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppiesApiApplication.class, args);
+    }
+
+}
